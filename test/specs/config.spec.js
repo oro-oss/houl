2001178dea187e531a460570b1851220b92b99ef
ktsn <ktsn55@gmail.com>
2017-01-31 19:05:28 +0900

    Tweak spec file

--- test/specs/config.spec.js
+++ test/specs/config.spec.js
@@ -1,45 +1,53 @@
 const path = require('path')
 const config = require('../../lib/config')
 
+const read = pathname => {
+  return config(path.join('test/fixtures', pathname))
+}
+
+// check
 const p = pathname => {
-  return path.relative(process.cwd(), pathname).replace(/\\/g, '/')
+  return path.relative(
+    path.join(process.cwd(), 'test/fixtures'),
+    pathname
+  ).replace(/\\/g, '/')
 }
 
 describe('Config', () => {
   it('loads js file', () => {
-    config('test/fixtures/normal.config.js')
+    read('normal.config.js')
   })
 
   it('loads json file', () => {
-    config('test/fixtures/test.config.json')
+    read('test.config.json')
   })
 
   it('throws if try loading other file', () => {
-    expect(() => config('test/fixtures/test.coffee'))
+    expect(() => read('test.coffee'))
       .toThrowError(/test\.coffee is non-supported file format/)
   })
 
   it('resolves input/output paths besed on cwd', () => {
-    const c = config('test/fixtures/normal.config.js')
-    expect(p(c.input)).toBe('test/fixtures/src')
-    expect(p(c.output)).toBe('test/fixtures/dist')
+    const c = read('normal.config.js')
+    expect(p(c.input)).toBe('src')
+    expect(p(c.output)).toBe('dist')
   })
 
   it('resolves paths in execute options based on cwd', () => {
-    const c = config('test/fixtures/normal.config.js')
+    const c = read('normal.config.js')
     const e = c.execute[0]
-    expect(p(e.input)).toBe('test/fixtures/src/img/_assets/*.svg')
-    expect(p(e.output)).toBe('test/fixtures/dist/img/share')
+    expect(p(e.input)).toBe('src/img/_assets/*.svg')
+    expect(p(e.output)).toBe('dist/img/share')
   })
 
   it('loads task file', () => {
-    const c = config('test/fixtures/normal.config.js')
+    const c = read('normal.config.js')
     expect(c.task.task1()).toBe('foo')
     expect(c.task.task2()).toBe('bar')
   })
 
   it('normalizes rules options', () => {
-    const c = config('test/fixtures/normal.config.js')
+    const c = read('normal.config.js')
     expect(typeof c.rules.png).toBe('object')
     expect(c.rules.png.task).toBe('imagemin')
   })
