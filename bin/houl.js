2b86bed875368238b7ca6e7213bd1dba2dc2b534
ktsn <ktsn55@gmail.com>
2017-02-03 09:28:17 +0900

    Initial implementation of dev command

--- bin/houl.js
+++ bin/houl.js
@@ -4,7 +4,7 @@ require('yargs') // eslint-disable-line
   .version()
   .usage('Usage: $0 <command> [options]')
   .command('build', 'Build your project with config and tasks', require('../lib/cli/build'))
-  .command('dev', 'Start development server') // TODO
+  .command('dev', 'Start development server', require('../lib/cli/dev'))
   .demandCommand(1)
   .help('h')
   .alias('h', 'help')
