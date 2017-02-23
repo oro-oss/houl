ef9002d38f47665fd9a90dc4555da67aa97b3df2
ktsn <ktsn55@gmail.com>
2017-02-24 01:02:35 +0900

    Search houl.config.js(on) if config option is not specified

--- test/specs/config.spec.js
+++ test/specs/config.spec.js
@@ -2,6 +2,7 @@
 
 const path = require('path')
 const loadConfig = require('../../lib/config').loadConfig
+const findConfig = require('../../lib/config').findConfig
 
 const read = pathname => {
   return loadConfig(path.join('test/fixtures', pathname))
@@ -19,5 +20,40 @@ describe('Config', () => {
   it('throws if try loading other file', () => {
     expect(() => read('test.coffee'))
       .toThrowError(/test\.coffee is non-supported file format/)
+  })
+
+  it('search config file', () => {
+    function exists (pathname) {
+      return '/path/houl.config.js' === pathname
+    }
+
+    expect(findConfig('/path/to/project', exists)).toBe('/path/houl.config.js')
+  })
+
+  it('also search json config file', () => {
+    function exists (pathname) {
+      return '/path/houl.config.json' === pathname
+    }
+
+    expect(findConfig('/path/to/project', exists)).toBe('/path/houl.config.json')
+  })
+
+  it('prefers js config', () => {
+    function exists (pathname) {
+      return [
+        '/path/to/houl.config.json',
+        '/path/to/houl.config.js'
+      ].indexOf(pathname) >= 0
+    }
+
+    expect(findConfig('/path/to/', exists)).toBe('/path/to/houl.config.js')
+  })
+
+  it('returns null if not found', () => {
+    function exists (pathname) {
+      return '/path/to/houl.config.json' === pathname
+    }
+
+    expect(findConfig('/path/other/project', exists)).toBe(null)
   })
 })
