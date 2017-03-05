5e078163724ced6fba4230f64585b758b8266a54
ktsn <ktsn55@gmail.com>
2017-03-05 20:28:10 +0900

    Update example to use dev and prod helpers

--- example/houl.task.js
+++ example/houl.task.js
@@ -1,10 +1,20 @@
+const { dev, prod } = require('../lib/api')
 const buble = require('gulp-buble')
 const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
 
 exports.js = stream => {
-  return stream.pipe(buble())
+  return stream
+    .pipe(dev(sourcemaps.init()))
+    .pipe(buble())
+    .pipe(prod(uglify()))
+    .pipe(dev(sourcemaps.write()))
 }
 
 exports.sass = stream => {
-  return stream.pipe(sass())
+  return stream
+    .pipe(dev(sourcemaps.init()))
+    .pipe(sass())
+    .pipe(dev(sourcemaps.write()))
 }
