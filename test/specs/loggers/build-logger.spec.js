963c6c02de9169766e18bfc118b7ac6732303b08
ktsn <ktsn55@gmail.com>
2017-04-24 15:11:49 +0900

    Stop rewrite already written logs

--- test/specs/loggers/build-logger.spec.js
+++ test/specs/loggers/build-logger.spec.js
@@ -3,8 +3,8 @@
 const Writable = require('stream').Writable
 const BuildLogger = require('../../../lib/loggers/build-logger')
 
-function createMockNow (interval) {
-  let count = 0
+function createMockNow (interval, base) {
+  let count = base || 0
   return () => {
     count += interval
     return count
@@ -12,7 +12,7 @@ function createMockNow (interval) {
 }
 
 function expectBuffer (buffer, expected) {
-  expect(buffer).toBe('START\n' + expected.join('\n') + '\n')
+  expect(buffer).toBe('START\n' + expected.concat(['']).join('\n'))
 }
 
 describe('Build Logger', () => {
@@ -23,33 +23,39 @@ describe('Build Logger', () => {
     mockStream = new Writable({
       write (chunk, encoding, done) {
         const str = chunk.toString()
-
-        if (str === '\u001b[1G' || str === '\u001b[1A') {
-          return done()
-        }
-
-        if (str === '\u001b[2K') {
-          buffer = buffer.replace(/(\n|^).*$/, '')
-        } else {
-          buffer += str
-        }
-
+        buffer += str
         done()
       }
     })
   })
 
-  it('should write processing files', () => {
-    const logger = new BuildLogger({ output: mockStream })
+  it('should write starting message', () => {
+    const logger = new BuildLogger({
+      output: mockStream,
+      now () {
+        return new Date(2017, 3, 24, 13, 31, 9).getTime()
+      }
+    })
 
-    logger.start('foo.js')
-    logger.start('bar.js')
-    logger.start('baz.js')
+    logger.startAll()
 
     expectBuffer(buffer, [
-      'Building\tfoo.js...',
-      'Building\tbar.js...',
-      'Building\tbaz.js...'
+      'Start building at 13:31:09'
+    ])
+  })
+
+  it('should write finish message', () => {
+    const logger = new BuildLogger({
+      output: mockStream,
+      now: createMockNow(10000, new Date(2017, 3, 24, 14, 30, 46).getTime())
+    })
+
+    logger.startAll()
+    logger.finishAll()
+
+    expectBuffer(buffer, [
+      'Start building at 14:30:56',
+      'Finish building at 14:31:06 10s'
     ])
   })
 
@@ -65,9 +71,23 @@ describe('Build Logger', () => {
     logger.finish('bar.scss', 'bar.css')
 
     expectBuffer(buffer, [
-      'Building\tfoo.js...',
-      'Finished\tbar.scss -> bar.css 0.2s',
-      'Building\tbaz.pug...'
+      'Finished\tbar.scss -> bar.css 200ms'
+    ])
+  })
+
+  it('should write multi-built files', () => {
+    const logger = new BuildLogger({
+      output: mockStream,
+      now: createMockNow(1000)
+    })
+
+    logger.start('foo.js')
+    logger.finish('foo.js', 'foo.js')
+    logger.finish('foo.js', 'foo.es6')
+
+    expectBuffer(buffer, [
+      'Finished\tfoo.js -> foo.js 1s',
+      'Finished\tfoo.js -> foo.es6 2s'
     ])
   })
 
@@ -81,34 +101,7 @@ describe('Build Logger', () => {
     logger.cache('foo.js')
 
     expectBuffer(buffer, [
-      'Cache exists\tfoo.js',
-      'Building\tbar.scss...'
-    ])
-  })
-
-  it('should apply buffers and remove them from head', () => {
-    const logger = new BuildLogger({
-      output: mockStream,
-      now: createMockNow(500)
-    })
-
-    logger.start('a.pug')
-    logger.start('b.pug')
-    logger.start('c.pug')
-    logger.start('d.pug')
-
-    expect(logger.buffer.length).toBe(4)
-
-    logger.finish('a.pug', 'a.html')
-    logger.cache('b.pug')
-    logger.finish('d.pug', 'd.html')
-
-    expect(logger.buffer.length).toBe(2)
-    expectBuffer(buffer, [
-      'Finished\ta.pug -> a.html 2s',
-      'Cache exists\tb.pug',
-      'Building\tc.pug...',
-      'Finished\td.pug -> d.html 1s'
+      'Cache exists\tfoo.js'
     ])
   })
 })
