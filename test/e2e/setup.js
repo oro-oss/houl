b6703d04b3edd2825122b5f424715eca12fadce5
ktsn <ktsn55@gmail.com>
2017-07-14 17:14:07 +0900

    Update e2e setup script

--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const spawn = require('child_process').spawn
 
-spawn('npm', ['install'], {
+spawn('npm', ['install', '--no-save'], {
   shell: true,
   cwd: path.resolve(__dirname, '../fixtures/e2e')
 })
