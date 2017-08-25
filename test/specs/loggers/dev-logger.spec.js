13bd0339d7de6a2689e9078119b5255069bbc7cb
katashin <ktsn55@gmail.com>
2017-08-25 18:02:39 +0900

    feat: print log while executing build, dev or watch command (#46)
    * feat: add build logger
    
    * chore: add comment about stream caveats
    
    * feat: add dev server logger
    
    * feat: add watch logger

---
+++ test/specs/loggers/dev-logger.spec.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const td = require('testdouble')
+const color = require('../../../lib/color')
+const DevLogger = require('../../../lib/loggers/dev-logger')
+
+describe('Dev Logger', () => {
+  it('should log that dev server starts', () => {
+    const console = { log: td.function() }
+    const logger = new DevLogger({}, {
+      console
+    })
+
+    logger.startDevServer(8080)
+    td.verify(console.log('Houl dev server is running at http://localhost:8080'))
+  })
+
+  it('should log that a source file is updated', () => {
+    const console = { log: td.function() }
+    const logger = new DevLogger({
+      input: '/path/to/input'
+    }, {
+      console
+    })
+
+    logger.updateFile('/path/to/input/js/index.js')
+
+    td.verify(console.log(color.yellow('UPDATED') + ' /js/index.js'))
+  })
+
+  it('should log that there is a GET request', () => {
+    const console = { log: td.function() }
+    const logger = new DevLogger({}, {
+      console
+    })
+
+    logger.getFile('/js/index.js')
+
+    td.verify(console.log(color.green('GET') + ' /js/index.js'))
+  })
+})
---
+++ test/specs/loggers/watch-logger.spec.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const td = require('testdouble')
+const color = require('../../../lib/color')
+const WatchLogger = require('../../../lib/loggers/watch-logger')
+
+describe('Watch Logger', () => {
+  it('should log watching source directory', () => {
+    const console = { log: td.function() }
+    const logger = new WatchLogger({
+      base: '/path/to',
+      input: '/path/to/input'
+    }, {
+      console
+    })
+
+    logger.startWatching()
+    td.verify(console.log('Houl is watching the source directory: input/'))
+  })
+
+  it('should log that a source file was updated', () => {
+    const console = { log: td.function() }
+    const logger = new WatchLogger({
+      base: '/path/to',
+      input: '/path/to/input'
+    }, {
+      console
+    })
+
+    logger.updateFile('/path/to/input/index.html')
+    td.verify(console.log(color.yellow('UPDATED') + ' /input/index.html'))
+  })
+
+  it('should log the dest path of the build', () => {
+    const console = { log: td.function() }
+    const logger = new WatchLogger({
+      base: '/path/to',
+      output: '/path/to/output'
+    }, {
+      console
+    })
+
+    logger.writeFile('/path/to/output/index.html')
+    td.verify(console.log(color.cyan('WROTE') + ' /output/index.html'))
+  })
+})
